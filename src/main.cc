658ceaf4e058aa06319ab275826c9d432861936d
Kevin Charles <kevin_ross_charles@live.com>
2016-08-15 23:32:44 +0100

    Complete create context demo
    Completed the demo where a window context is created and lauched.

--- src/main.cc
+++ src/main.cc
@@ -1,17 +1,46 @@
-// GLEW
+#include <stdlib.h>
 #define GLEW_STATIC
 #include <GL/glew.h>
-// GLFW
 #include <GLFW/glfw3.h>
+#include <iostream>
 
+int main() {
+  // Attempt to init glfw fail if error
+  if (!glfwInit())
+    exit(EXIT_FAILURE);
 
-int main()
-{
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+  // Create a GLFWwindow with our desired settings
+  int winWidth = 1024, winHeight = 768;
+  GLFWwindow *window = nullptr;
+  window = glfwCreateWindow(winWidth, winHeight, "glfw", NULL, NULL);
 
-    return 0;
+  // Ensure window we created is valid
+  if (!window) {
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+
+  // Make our created window the current context
+  glfwMakeContextCurrent(window);
+
+  std::cout << "Hello world!" << std::endl;
+
+  // ------- MAIN LOOP -------------------------------------------------------
+  while (!glfwWindowShouldClose(window)) {
+
+    glViewport(0, 0, winWidth, winHeight); // set view port in pixels
+    glClearColor(1, 0, 0, 1);              // clear window contents
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // put drawing code here
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  // Destroy the window and terminate glfw
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
+  return 0;
 }
